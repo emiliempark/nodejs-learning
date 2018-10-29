713cfe1ae8ca0970ec9da3e718bbea0e665f26eb
Emilie Park <emilie@oneroom.co.nz>
2018-10-29 17:04:45 +1300

    Introduced URL module

--- http.js
+++ http.js
@@ -1,14 +1,30 @@
 var http = require('http');
 var fs = require('fs'); //file system
+var url = require('url');
 
-var server = http.createServer(function(req, res){
-	fs.readFile('example.html', function(error, data){
-	    res.writeHead(200, {'Content-Type': 'text/html'});
-	    res.end(data);
+http.createServer(function(req, res){
+	// parsing file/folder name
+	var pathname = url.parse(req.url).pathname;
+	console.log("Request for " + pathname + " received.");
 
+	//set default readFile
+   if(pathname=="/"){
+       pathname = "/index.html";
+   }
+
+	// read file
+	fs.readFile(pathname.substr(1), function(err, data){ // load matching file name with url
+
+		if(err){ // page not found
+			console.log(err);
+			res.writeHead(404, { 'content-type': 'text/html'});
+		}else{ // page found
+			res.writeHead(200, { 'content-type': 'text/html'}); // write head
+			res.write(data.toString()); // write body
+		}
+		// send date to responsebody
+		res.end();
 	});
-});
+}).listen(9000);
 
-server.listen(9000, function(){
-    console.log('server is listening on port number 9000');
-});
+console.log('server is listening on port number 9000');
