673644903e8a395faf4af4bf69f7e95df0685705
EmiliePark <emiliempark@gmail.com>
2019-12-06 22:31:35 +1300

    Run first node js + Express server

--- http.js
+++ http.js
@@ -1,14 +1,21 @@
 var http = require('http');
 var fs = require('fs'); //file system
 
-var server = http.createServer(function(req, res){
-	fs.readFile('example.html', function(error, data){
-	    res.writeHead(200, {'Content-Type': 'text/html'});
-	    res.end(data);
+var server = require('./server');
 
-	});
-});
+var httpServer = http.createServer(server);
 
-server.listen(9000, function(){
-    console.log('server is listening on port number 9000');
-});
+// var server = http.createServer(function(req, res){
+// 	fs.readFile('example.html', 'utf8', function(error, data){
+// 	    res.writeHead(200, {'Content-Type': 'content-type:application/javascript'});
+// 	    res.end(data);
+
+// 	});
+// });
+
+// server.listen(9000, function(){
+//     console.log('server is listening on port number 9000');
+// });
+
+
+httpServer.listen(8080);
