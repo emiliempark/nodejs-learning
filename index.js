22e9e4d50919be4896eb20f9c861e56debc2be5f
Emilie <emiliempark@gmail.com>
2020-01-11 13:36:28 +1300

    Load js file

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log('index.js loaded');
+
+var btns = [
+  document.getElementById('item1'),
+  document.getElementById('item2'),
+  document.getElementById('item3'),
+  document.getElementById('item4'),
+  document.getElementById('item5'),
+  document.getElementById('item6'), 
+];
